c46b273e1d9ffa98d7a0487f2f561aa5c1840768
Emma <emmajcool@outlook.com>
2023-07-18 14:51:04 -0700

    Initial Commit

---
+++ Eco Educator/Eco_EducatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  Eco_EducatorApp.swift
+//  Eco Educator
+//
+//  Created by Scholar on 7/18/23.
+//
+
+import SwiftUI
+
+@main
+struct Eco_EducatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
